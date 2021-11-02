e366fb6d79ead0c3b2efc5cc32ac184602063a7e
orbita2d <orbita@orbital.me.uk>
2021-11-02 23:44:51 +0000

    Comments

--- clifford/attractor.py
+++ clifford/attractor.py
@@ -5,6 +5,8 @@ from typing import Tuple
 
 @jit
 def increment_clifford(x: float, y: float, a: float, b: float, c: float, d: float):
+    """ Take a single point, increment it following the clifford attractor equations."""
+    # Rescale so that attractor fits in [-1, 1]
     x_width = 1 + np.abs(c)
     y_width = 1 + np.abs(d)
     xn = (np.sin(a*y*y_width) + c * np.cos(a*x*x_width)) / x_width
@@ -14,12 +16,15 @@ def increment_clifford(x: float, y: float, a: float, b: float, c: float, d: floa
 
 @jit
 def build_clifford(x: np.ndarray, y: np.ndarray, x0: float, y0: float, a: float, b: float, c: float, d: float, count: int):
+    """Iterate through many points, producing array of points on attractor"""
     x[0], y[0] = increment_clifford(x0, y0, a, b, c, d)
     for i in range(1, count):
         x[i], y[i] = increment_clifford(x[i - 1], y[i - 1], a, b, c, d)
 
 
 class ArrayCounts:
+    """ Class for density array. Poitns are binned into pixels so we can make an image.
+     Also track the change in x, and y for renderers."""
     def __init__(self, size: Tuple[int, int], padding: float):
         self.image_size: Tuple[int, int] = size
         self.real_size: int = min(size[0], size[1])
@@ -36,6 +41,7 @@ class ArrayCounts:
 
 
 def update_counts(x0: float, y0: float, x: np.ndarray, y: np.ndarray, arr: ArrayCounts):
+    """ Take array of points on attractor, append to density array with the size we've been given."""
     lpad: float = max(arr.image_size[0] - arr.image_size[1], 0) / 2 + arr.padding * arr.size()
     tpad: float = max(arr.image_size[1] - arr.image_size[0], 0) / 2 + arr.padding * arr.size()
     scale = arr.size() * (1 - 2 * arr.padding) / 2
@@ -50,6 +56,7 @@ def update_counts(x0: float, y0: float, x: np.ndarray, y: np.ndarray, arr: Array
 
 
 def test_closed(p: np.ndarray):
+    """ Check if a particular parameter is a small orbit which will make a bad image."""
     x0 = 0.08
     y0 = 0.12
     count = 400000
@@ -67,6 +74,8 @@ def test_closed(p: np.ndarray):
 
 
 def get_frame(x0: float, y0: float, p: np.ndarray, count: int, arr: ArrayCounts):
+    """ Take some initial points in x, y and point in parameter space, increment the clifford atttractor count times
+    and append them to the density array """
     x = np.zeros(count, dtype=float)
     y = np.zeros(count, dtype=float)
     build_clifford(x, y, x0, y0, p[0], p[1], p[2], p[3], count)
--- main.py
+++ main.py
@@ -15,20 +15,26 @@ debug = False
 
 
 def gifsicle_cleanup(input: str, output: str):
+    """ Make the gif have the right framerate, loop, etc"""
     os.system(f'gifsicle -i -l0 -d2 -O3 -j8 --colors 256 {input} -o {output}')
 
 
 def make_gifsicle(images_path: str, output: str):
+    """ Make a gif."""
     os.system(f'gifsicle --colors 256 -m {os.path.join(images_path, "frame*.gif")} -o {output}')
 
 
 def make_mp4(images_path: str, output: str):
+    """ Make an mp4 from <images_path>/frame%0d.png frames. Save to <output>"""
     cmd = f'ffmpeg -y -loglevel 0 -r 50 -f image2 -i {os.path.join(images_path, "frame%04d.png")} -vcodec libx264 ' \
           f'-crf 15  -pix_fmt yuv420p {output} '
     os.system(cmd)
 
 
-def solve_frame(fi: int, blocks: int, n: int, generator: clifford.GeneratorSettings, renderer: clifford.RenderSettings, size: Tuple[int, int]):
+def solve_frame(fi: int, blocks: int, n: int, generator: clifford.GeneratorSettings, renderer: clifford.RenderSettings,
+                size: Tuple[int, int]):
+    """ Take a frame count, number of iterations. Calculate clifford attractor point density, render as image,
+    save frame. """
     counts = clifford.ArrayCounts(size, 0.05)
     x0 = 0.08
     y0 = 0.12
@@ -60,14 +66,16 @@ def solve_frame(fi: int, blocks: int, n: int, generator: clifford.GeneratorSetti
 
 
 if __name__ == '__main__':
-    if len(sys.argv) <= 5:
-        print('config blocks frames size cores')
+    if len(sys.argv) <= 6:
+        print(f'{sys.argv[0]} <config> <blocks> <frames> <width> <height> <cores>')
         exit(1)
     config_path = sys.argv[1]
     blocks = int(sys.argv[2])
     frames = int(sys.argv[3])
     size = (int(sys.argv[5]), int(sys.argv[4]))
     cores = int(sys.argv[6])
+
+    # Read settings from json file
     with open(config_path, 'r') as f:
         json_data: dict = json.load(f)
         name = json_data["name"]
@@ -77,6 +85,8 @@ if __name__ == '__main__':
         render_settings = clifford.get_renderer(json_data["data"]["render"])
 
     print(f'Building {name}.gif')
+
+    # Delete old frames.
     frame_list = glob.glob(os.path.join(output_location, 'frame*.gif'))
     for frame in frame_list:
         os.remove(frame)
@@ -84,20 +94,26 @@ if __name__ == '__main__':
     for frame in frame_list:
         os.remove(frame)
 
+    # Helper function, so we can just pass a frame index. Makes the multiprocessing clearer.
     def solve_frame_p(x):
         solve_frame(x, blocks, frames, generator_settings, render_settings, size)
 
     t = time.perf_counter()
     if debug:
+        # Just one frame at a time.
+        # Also, mp can squash error messages sometimes.
         for i in range(frames):
             solve_frame_p(i)
     else:
+        # Iterate over frames on multiple cores.
         with multiprocessing.Pool(cores) as pool:
             pool.imap(solve_frame_p, range(frames))
             pool.close()
             pool.join()
     time_used = time.perf_counter() - t
     print(f'Done: {frames} frames in {int(time_used/60):d} min {time_used % 60:.1f}s')
+
+    # Save stuff
     print(f'Rendering gif')
     convert_out = os.path.join(output_location, f'{name}.gif')
     make_gifsicle(output_location, convert_out)
